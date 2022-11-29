7492dba224a37b4792b4264da545ffcc9643905c
Luca Di Maio <luca.dimaio1@gmail.com>
2022-11-29 14:02:53 +0100

    Remove leftovers
    Signed-off-by: Luca Di Maio <luca.dimaio1@gmail.com>

--- dbus-notification.sh
+++
@@ -1,7 +0,0 @@
-gdbus call --system   \
-   --dest org.opensuse.tukit.Updated \
-   --object-path /org/opensuse/tukit/Updated \
-   --method org.opensuse.tukit.Updated.Notify \
-  success
-  # failure
-  # success
